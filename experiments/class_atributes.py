95f55d156c121eac6e9b97384d892ca16b52ae90
GabrielStoica <gabriel.stoica0511@gmail.com>
2020-10-06 12:38:08 +0300

    added new experiment + minor comment addition

--- experiments/class_atributes.py
+++ experiments/class_atributes.py
@@ -1,4 +1,5 @@
 class Contact:
+    """Example of a class which has a variable editable by its instances."""
     all_contacts = []
 
     def __init__(self, name, email):
@@ -8,6 +9,7 @@ class Contact:
 
 
 class Color:
+    """Use the property() call to create a class property."""
     def __init__(self, rgb_value, name):
         self.rgb_value = rgb_value
         self._name = name
@@ -23,6 +25,23 @@ class Color:
     name = property(_get_name, _set_name)
 
 
+class Person:
+    """Try using the @property decorator instead."""
+    def __init__(self, age: int, name: str):
+        self.age = age
+        self._name = name
+
+    # The getter is always first, thus is used to declare the property. Name of
+    # the getter is the same as the name of the property.
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+
 if __name__ == "__main__":
     # Class variables can be modified from object-level calls.
     contact = Contact("John", "john@example.com")
@@ -33,3 +52,7 @@ if __name__ == "__main__":
     color = Color('#ffffff', 'black')
     print(color.name)
     color.name = 'white'
+
+    # Class which uses the property decorator
+    person = Person(age=25, name='John')
+    print(person.name)
